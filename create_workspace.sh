8118577a527f9ef16318752d14acf0749be030fc
Unknown <>
2018-12-17 21:26:38 +0900

    Add Dockerfile and docker-compose

---
+++ create_workspace.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+mkdir workspace
+mkdir workspace/calc
+mkdir workspace/codebox
+mkdir workspace/gitbucket
+mkdir workspace/mongo
+mkdir workspace/mysql
+mkdir workspace/redmine
+mkdir workspace/redmine/files
+mkdir workspace/redmine/plugins
+mkdir workspace/redmine/themes
